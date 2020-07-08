f7e9cb4eb288aa1c5867773d27431e013b652dd6
Siberian Nerd <whatsgoin1@gmail.com>
2020-07-08 23:34:56 +0300

    изменение для цепи второго порядка

--- generator.py
+++ generator.py
@@ -8,13 +8,17 @@ def generate_random_start(model):
 def generate_random_sentence(markov_model):
     current_word = generate_random_start(markov_model)
     sentence = [current_word]
-    sentence_lenght = random.randint(3, 20)
+    sentence_lenght = random.randint(3, 10)
     for i in range(0, sentence_lenght):
         current_dictogram = markov_model[current_word]
         random_weighted_word = current_dictogram.return_weight_random_word()
-        current_word = random_weighted_word
-        sentence.append(current_word)
+        # вот эта часть убирается, если цепь 1 порядка
+        for word in random_weighted_word:
+            sentence.append(word)
+        current_word = list(random_weighted_word)[1] 
+        # current_word = random_weight_word - если цепь первого порядка
+        # sentence.append(current_word)
     sentence[0] = sentence[0].capitalize()
     return " ".join(sentence) + ". "
-    return sentence
+
     
--- models.py
+++ models.py
@@ -3,11 +3,12 @@ from dictogram import Dictogram
 def make_markov_model(data):
     markov_model = dict()
 
-    for i in range(0, len(data)-1):
+    for i in range(0, len(data)-2):
         if data[i] in markov_model:
-            markov_model[data[i]].update([data[i+1]])
+            markov_model[data[i]].update([(data[i+1], data[i+2])])
+            #markov_model[data[i]].update([data[i+1]]) # чтобы сделать цепь 1 порядка
         else:
-            markov_model[data[i]] = Dictogram([data[i+1]])
-
+            markov_model[data[i]]= Dictogram([(data[i+1], data[i+2])])
+            #markov_model[data[i]]= Dictogram([data[i+1]]) # чтобы сделать цепь 1 порядка
     return markov_model
 
