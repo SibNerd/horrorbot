0a1e4fce3790c3f70bf87c20300c5fffac344c50
Siberian Nerd <whatsgoin1@gmail.com>
2020-10-16 16:08:59 +0300

    clear code

--- textmaker.py
+++ textmaker.py
@@ -7,22 +7,14 @@ import random
 def create_model():
     with codecs.open('original.txt', 'r', encoding='utf-8') as scource_file: 
         original_text = scource_file.read()
-    total_text = []
     scource_text = re.split(r' ', original_text)
     text_model = models.make_markov_model(scource_text)
     return text_model
 
 def create_text(model):
     total_text = []
-    lenght = random.randint(1, 5)
+    lenght = random.randint(1, 10)
     for i in range(lenght):
         sentence = generator.generate_random_sentence(model)
         total_text.append(sentence)
     return total_text
-
-"""
-# writing generated text
-with codecs.open('words.txt', 'w', encoding='utf-8') as total_words:
-    for sentence in total_text:
-        total_words.write(sentence)
-"""
