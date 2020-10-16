f490dd71fa983b48f8b6a5a8a2adb69f4d627320
Siberian Nerd <whatsgoin1@gmail.com>
2020-10-16 16:08:19 +0300

    add generation of the right first word

--- generator.py
+++ generator.py
@@ -1,13 +1,22 @@
-from dictogram import Dictogram
-import random
-from collections import deque
+import random, re
+
+
 
 def generate_random_start(model):
     return random.choice(list(model.keys()))
 
+def generate_start_word(model):
+    while True:
+        start_word = random.choice(list(model.keys()))
+        result = re.search(r'[.,?!]', start_word)
+        if result:
+            return start_word
+            break
+
+
 def generate_random_sentence(markov_model):
-    current_word = generate_random_start(markov_model)
-    sentence = [current_word]
+    current_word = generate_start_word(markov_model)
+    sentence = []
     sentence_lenght = random.randint(3, 10)
     for i in range(0, sentence_lenght):
         current_dictogram = markov_model[current_word]
